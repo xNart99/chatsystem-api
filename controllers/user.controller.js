aa57500f88c67294eef7f9d2e23c5548354c9f43
xNart99 <kiradz1328@gmail.com>
2022-09-25 17:09:26 +0700

    feature: get all user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,6 +11,18 @@ const updateRoleUser = async (req, res) => {
     }
 }
 
+const getAllUser = async (req, res) => {
+    try {
+        const users = await userService.getAllUsers();
+
+        return res.status(200).json(users);
+    }catch(error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal server error!"});
+    }
+}
+
 module.exports = {
-    updateRoleUser
+    updateRoleUser,
+    getAllUser
 }
--- routers/user.router.js
+++ routers/user.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.put('/role', userController.updateRoleUser);
-
+router.get('/', userController.getAllUser);
 
 
 module.exports = router;
